7b544f4fb9f298bb3c72ce6bec8f97cb5655bb9d
Yuri D. Lensky <ydl@mit.edu>
2013-08-01 18:34:54 -0400

    Improved functionality of supplemental operators.

--- generic-math/operators/supplemental-operators.lisp
+++ generic-math/operators/supplemental-operators.lisp
@@ -1,9 +1,27 @@
 (in-package #:monolith)
 
-;; Defines supplemental elementary operators that do not exist in scheme as simple primitives
+;; Defines supplemental elementary operators that do not exist in Common Lisp as simple primitives
 
-(defun g/sec (x) (g// (g/cos x)))
-(defun g/csc (x) (g// (g/sin x)))
-(defun g/cot (x) (g// (g/tan x)))
+(defmgenop g/sec (x))
+(defmethod g/sec ((x number))
+  (/ (cos x)))
+(defmethod g/sec ((x expression))
+  (make-g/sec-expr x))
+(defmethod g/sec ((x symbol))
+  (make-g/sec-expr x))
 
-(defun g/abs (x) (g/sqrt (g/expt x 2)))
+(defmgenop g/csc (x))
+(defmethod g/csc ((x number))
+  (/ (sin x)))
+(defmethod g/csc ((x expression))
+  (make-g/csc-expr x))
+(defmethod g/csc ((x symbol))
+  (make-g/csc-expr x))
+
+(defmgenop g/cot (x))
+(defmethod g/cot ((x number))
+  (/ (tan x)))
+(defmethod g/cot ((x expression))
+  (make-g/cot-expr x))
+(defmethod g/cot ((x symbol))
+  (make-g/cot-expr x))
